0298d0d447e91eca604658c1c5d5ed8fc65792c5
zhangwei <245370344@qq.com>
2018-11-28 22:26:16 +0800

    update

--- spring-boot-websocket/src/test/java/com/example/springbootwebsocket/SpringBootWebsocketApplicationTests.java
+++ spring-boot-websocket/src/test/java/com/example/springbootwebsocket/SpringBootWebsocketApplicationTests.java
@@ -5,12 +5,29 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Map;
+import java.util.Set;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootWebsocketApplicationTests {
 
 	@Test
 	public void contextLoads() {
-	}
 
+	}
 }
